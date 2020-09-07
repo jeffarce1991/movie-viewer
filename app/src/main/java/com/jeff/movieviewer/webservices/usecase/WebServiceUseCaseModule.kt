604acdf19209f23d72a9d3d7ea9565aaa92f1a8b
jeff <jeffandrew.s.arce@gmail.com>
2020-09-07 11:25:12 +0800

    Binds MovieLoader.

--- app/src/main/java/com/jeff/movieviewer/webservices/usecase/WebServiceUseCaseModule.kt
+++ app/src/main/java/com/jeff/movieviewer/webservices/usecase/WebServiceUseCaseModule.kt
@@ -1,6 +1,8 @@
 package com.jeff.movieviewer.webservices.usecase
 
+import com.jeff.movieviewer.webservices.usecase.loader.DefaultMovieRemoteLoader
 import com.jeff.movieviewer.webservices.usecase.loader.DefaultPhotoRemoteLoader
+import com.jeff.movieviewer.webservices.usecase.loader.MovieRemoteLoader
 import com.jeff.movieviewer.webservices.usecase.loader.PhotoRemoteLoader
 import dagger.Binds
 import dagger.Module
@@ -12,4 +14,8 @@ interface WebServiceUseCaseModule {
     fun bindPhotoRemoteLoader(
             defaultPhotoRemoteLoader: DefaultPhotoRemoteLoader):
             PhotoRemoteLoader
+    @Binds
+    fun bindMovieRemoteLoader(
+            defaultMovieRemoteLoader: DefaultMovieRemoteLoader
+    ): MovieRemoteLoader
 }
