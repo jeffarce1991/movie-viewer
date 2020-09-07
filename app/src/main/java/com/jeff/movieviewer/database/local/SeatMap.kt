43db979d1f09fe21e84fdfcff94032b9e5bb7999
jeff <jeffandrew.s.arce@gmail.com>
2020-09-08 06:18:47 +0800

    Add SeatMap Model.

---
+++ app/src/main/java/com/jeff/movieviewer/database/local/SeatMap.kt
@@ -0,0 +1,30 @@
+package com.jeff.movieviewer.database.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = SeatMap.TABLE_NAME)
+data class SeatMap constructor(
+    @ColumnInfo(name = "id")
+    @PrimaryKey(autoGenerate = true)
+    var id: String?,
+    @ColumnInfo(name = "seatmap")
+    var seatmap: List<List<String>>,
+    @ColumnInfo(name = "genre")
+    var availableSeats: AvailableSeats) {
+
+    constructor() : this("", listOf<List<String>>(listOf()), AvailableSeats())
+
+    companion object {
+        const val COLUMN_ID = "id"
+        const val TABLE_NAME = "seatmaps"
+    }
+}
+
+data class AvailableSeats (
+    var available: List<String>,
+    var seatCount: Int
+) {
+    constructor() : this(listOf(), -1)
+}
