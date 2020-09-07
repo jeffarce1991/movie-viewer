eb902d234af28480ec54c8208b52498177c72840
jeff <jeffandrew.s.arce@gmail.com>
2020-09-08 06:18:16 +0800

    Add & Implement loadSeatMap on MovieLoader. Add mapper for SeatMap and AvailableSeats classes.

--- app/src/main/java/com/jeff/movieviewer/supplychain/photo/DefaultMovieLoader.kt
+++ app/src/main/java/com/jeff/movieviewer/supplychain/photo/DefaultMovieLoader.kt
@@ -1,9 +1,13 @@
 package com.jeff.movieviewer.supplychain.photo
 
+import com.jeff.movieviewer.database.local.AvailableSeats
 import com.jeff.movieviewer.database.local.Movie
 import com.jeff.movieviewer.database.local.Photo
+import com.jeff.movieviewer.database.local.SeatMap
 import com.jeff.movieviewer.main.mapper.PhotoDtoToPhotoMapper
+import com.jeff.movieviewer.webservices.dto.AvailableSeatsDto
 import com.jeff.movieviewer.webservices.dto.MovieDto
+import com.jeff.movieviewer.webservices.dto.SeatMapDto
 import com.jeff.movieviewer.webservices.internet.RxInternet
 import com.jeff.movieviewer.webservices.usecase.loader.MovieRemoteLoader
 import io.reactivex.Observable
@@ -19,6 +23,28 @@ constructor(private val remoteLoader: MovieRemoteLoader,
             .map { mapMovieDtoToMovie(it) }
             //.flatMap { Single.fromObservable(localDetailsSaver.save(it)) }
             .flatMap { Single.just(it) }
+    }
+
+    override fun loadSeatMap(): Single<SeatMap> {
+        return remoteLoader.loadSeatMap()
+            .map { mapSeatMapDtoToSeatMap(it) }
+            //.flatMap { Single.fromObservable(localDetailsSaver.save(it)) }
+            .flatMap { Single.just(it) }
+    }
+
+    private fun mapSeatMapDtoToSeatMap(dto: SeatMapDto): SeatMap {
+        return SeatMap(
+            "-1",
+            dto.seatmap,
+            availableSeatDtoToAvailableSeats(dto.availableSeats)
+        )
+    }
+
+    private fun availableSeatDtoToAvailableSeats(dto: AvailableSeatsDto): AvailableSeats {
+        return AvailableSeats(
+            dto.seats,
+            dto.seatCount
+        )
     }
 
     private fun mapMovieDtoToMovie(dto: MovieDto): Movie {
--- app/src/main/java/com/jeff/movieviewer/supplychain/photo/MovieLoader.kt
+++ app/src/main/java/com/jeff/movieviewer/supplychain/photo/MovieLoader.kt
@@ -2,9 +2,11 @@ package com.jeff.movieviewer.supplychain.photo
 
 import com.jeff.movieviewer.database.local.Movie
 import com.jeff.movieviewer.database.local.Photo
+import com.jeff.movieviewer.database.local.SeatMap
 import io.reactivex.Single
 
 interface MovieLoader {
 
     fun loadMovie(): Single<Movie>
+    fun loadSeatMap(): Single<SeatMap>
 }
