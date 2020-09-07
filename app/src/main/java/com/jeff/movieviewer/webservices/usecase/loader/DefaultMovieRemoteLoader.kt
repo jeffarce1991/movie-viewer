d3b0534ab1271e133b63a128fe1ce3c07a9af2d7
jeff <jeffandrew.s.arce@gmail.com>
2020-09-08 06:16:00 +0800

    Add & Implement loadSeatMap() in MovieRemoteLoader.

--- app/src/main/java/com/jeff/movieviewer/webservices/usecase/loader/DefaultMovieRemoteLoader.kt
+++ app/src/main/java/com/jeff/movieviewer/webservices/usecase/loader/DefaultMovieRemoteLoader.kt
@@ -5,6 +5,7 @@ import com.jeff.movieviewer.webservices.api.photos.MovieApi
 import com.jeff.movieviewer.webservices.api.photos.PhotosApi
 import com.jeff.movieviewer.webservices.dto.MovieDto
 import com.jeff.movieviewer.webservices.dto.PhotoDto
+import com.jeff.movieviewer.webservices.dto.SeatMapDto
 import com.jeff.movieviewer.webservices.transformer.ResponseCodeNot200SingleTransformer
 import io.reactivex.Single
 import javax.inject.Inject
@@ -15,6 +16,14 @@ constructor(private val apiFactory: ApiFactory): MovieRemoteLoader {
     override fun loadMovie(): Single<MovieDto> {
         return apiFactory.create(MovieApi::class.java)
             .flatMap { it.loadMovie() }
+            .compose(ResponseCodeNot200SingleTransformer())
+            .flatMap { response ->
+                Single.just(response.body()!!) }
+    }
+
+    override fun loadSeatMap(): Single<SeatMapDto> {
+        return apiFactory.create(MovieApi::class.java)
+            .flatMap { it.loadSeatMap() }
             .compose(ResponseCodeNot200SingleTransformer())
             .flatMap { response ->
                 Single.just(response.body()!!) }
--- app/src/main/java/com/jeff/movieviewer/webservices/usecase/loader/MovieRemoteLoader.kt
+++ app/src/main/java/com/jeff/movieviewer/webservices/usecase/loader/MovieRemoteLoader.kt
@@ -2,9 +2,11 @@ package com.jeff.movieviewer.webservices.usecase.loader
 
 import com.jeff.movieviewer.webservices.dto.MovieDto
 import com.jeff.movieviewer.webservices.dto.PhotoDto
+import com.jeff.movieviewer.webservices.dto.SeatMapDto
 import io.reactivex.Single
 
 interface MovieRemoteLoader {
 
     fun loadMovie(): Single<MovieDto>
+    fun loadSeatMap(): Single<SeatMapDto>
 }
