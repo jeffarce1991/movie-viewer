09e495652226c26518985ab0ee74c984be99bb51
jeff <jeffandrew.s.arce@gmail.com>
2020-09-08 23:52:45 +0800

    Add ScheduleDto to Schedule Mappers.

--- app/src/main/java/com/jeff/movieviewer/supplychain/photo/DefaultMovieLoader.kt
+++ app/src/main/java/com/jeff/movieviewer/supplychain/photo/DefaultMovieLoader.kt
@@ -32,16 +32,98 @@ constructor(private val remoteLoader: MovieRemoteLoader,
             //.flatMap { Single.fromObservable(localDetailsSaver.save(it)) }
             .flatMap { Single.just(it) }
     }
+
+    private fun mapScheduleDtoToSchedule(it: ScheduleDto): Schedule {
+        return Schedule(
+            "-1",
+            mapDatesDtoToDates(it.dates),
+            mapCinemasDtoToCinemas(it.cinemas),
+            mapTimesDtoToTimes(it.times)
+        )
+    }
+
+    private fun mapDatesDtoToDates(dtoList: List<DateDto>): List<Schedule.Date> {
+        val list : MutableList<Schedule.Date> = mutableListOf()
+        for (d in dtoList) {
+            list.add(Schedule.Date(d.id, d.label, d.date))
+        }
+        return list
+    }
+
+    /* CINEMAS */
+    private fun mapCinemasDtoToCinemas(dtoList: List<CinemasDto>)
+            : List<Schedule.Cinemas> {
+        val list : MutableList<Schedule.Cinemas> = mutableListOf()
+        for (c in dtoList) {
+            list.add(Schedule.Cinemas(c.parent, mapCCtoToCC(c.cinemas)))
+        }
+        return list
+    }
+
+    /* CC = Cinemas.Cinemas */
+    private fun mapCCtoToCC(dtoList: List<CinemaDto>)
+            : List<Schedule.Cinemas.Cinema> {
+        val list : MutableList<Schedule.Cinemas.Cinema> = mutableListOf()
+        for (c in dtoList) {
+            list.add(Schedule.Cinemas.Cinema(c.id, c.cinemaId, c.label))
+        }
+        return list
+    }
+
+    /* TIMES */
+    private fun mapTimesDtoToTimes(dtoList: List<TimesDto>)
+            : List<Schedule.Times> {
+        val list : MutableList<Schedule.Times> = mutableListOf()
+        val ttList : MutableList<Schedule.Times.Time> = mutableListOf()
+        for (t in dtoList) {
+            for (tt in t.times) {
+                ttList.add(Schedule.Times.Time(
+                    tt.id,
+                    tt.label,
+                    tt.scheduleId,
+                    tt.popcornPrice,
+                    tt.popcornLabel,
+                    tt.seatingType,
+                    tt.price,
+                    tt.variant
+                ))
+            }
+            list.add(Schedule.Times(t.parent, ttList))
+        }
+        return list
+    }
+
+    /* TT = Times.Times */
+    private fun mapTTtoToTT(dtoList: List<TimeDto>)
+            : List<Schedule.Times.Time> {
+        val list : MutableList<Schedule.Times.Time> = mutableListOf()
+        for (t in dtoList) {
+            list.add(Schedule.Times.Time(
+                t.id,
+                t.label,
+                t.scheduleId,
+                t.popcornPrice,
+                t.popcornLabel,
+                t.seatingType,
+                t.price,
+                t.variant
+            ))
+        }
+        return list
+    }
+
+    /* SEATMAP */
     private fun mapSeatMapDtoToSeatMap(dto: SeatMapDto): SeatMap {
         return SeatMap(
             "-1",
             dto.seatmap,
-            availableSeatDtoToAvailableSeats(dto.availableSeats)
+            mapAvailableSeatDtoToAvailableSeats(dto.availableSeats)
         )
     }
 
-    private fun availableSeatDtoToAvailableSeats(dto: AvailableSeatsDto): AvailableSeats {
-        return AvailableSeats(
+
+    private fun mapAvailableSeatDtoToAvailableSeats(dto: AvailableSeatsDto): SeatMap.AvailableSeats {
+        return SeatMap.AvailableSeats(
             dto.seats,
             dto.seatCount
         )
@@ -58,7 +140,8 @@ constructor(private val remoteLoader: MovieRemoteLoader,
             dto.runtimeMins,
             dto.releaseDate,
             dto.synopsis,
-            dto.cast
+            dto.cast,
+            dto.theater
         )
     }
 }
